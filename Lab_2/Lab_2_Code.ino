df14540efd4f15974f710df46c3eb7265dde303e
Hoffer-Jacob <73317702+Hoffer-Jacob@users.noreply.github.com>
2021-02-12 10:15:47 -0700

    New dir

---
+++ Lab_2/Lab_2_Code.ino
@@ -0,0 +1,131 @@
+
+/* Dead Reckoning
+  20200117
+  Program to get the ardbot to drive a predefined path
+  jsteele, mshapiro, klarsen
+*/
+
+// Complete Tasks in Initial_Robot_Testing.ino first!!
+
+/* Program TODO LIST
+  1) Change the milliSecondsPerCM constant to the value you found in testing
+  2) Change the milliSecondsPer90Deg constant to the value you found in testing
+  3) Change the PWM of the forward function to the values you found in testing
+  4) Write code for the button pause functionality
+  5) Write your own turn function
+
+  Note: type // to make a single line comment
+        Comments are for the future you to understand how you wrote your code
+*/
+
+// Preprocessor Definitions
+#define FORWARD  0
+#define LEFT     1
+#define RIGHT   -1
+#define pushButton 2
+#define A        1
+#define B        2
+#define pwmA    3
+#define dirA    12
+#define pwmB    11
+#define dirB    13
+
+// the following converts centimeters into milliseconds as long datatype
+#define milliSecondsPerCM  22 //CHANGE THIS ACCORDING TO YOUR BOT
+#define milliSecondsPer90Deg 185 //CHANGE THIS ACCORDNING TO YOUR BOT
+
+// the itemized list of moves for the robot as a 1D array
+// this setup assumes that all the turns are 90 degrees and that all motions are pairs of drives and turns.
+int moves[] = {140, LEFT, 90, RIGHT, 60, RIGHT, 180, RIGHT, 100, RIGHT, 60, RIGHT, 100, RIGHT, 150, LEFT};
+
+void setup() {
+  // set up the motor drive ports
+  pinMode(pwmA, OUTPUT);
+  pinMode(dirA, OUTPUT);
+  pinMode(pwmB, OUTPUT);
+  pinMode(dirB, OUTPUT);
+  // make the pushbutton's pin an input:
+  pinMode(pushButton, INPUT); //CHANGE TO INPUT_PULLUP
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+void loop() {
+  int i, dist, dir;
+  long time;
+  while (digitalRead(pushButton) == 1);
+  while (digitalRead(pushButton) == 0);
+  //This for loop steps (or iterates) through the array 'moves'
+  for (i = 0; i < sizeof(moves) / 2; i = i + 2) {
+
+    /* Write code here to make the program pause until
+       the button has been pressed. Remember that getting a value
+       from the board is digitalRead(pinNumber) and if statements
+       need a double equals sign (==) for comparisons
+    */
+
+    delay(250);
+    //Forward Leg of each step
+    Serial.print("Step #:");
+    Serial.println(i);
+    dist = moves[i];
+    Serial.print("Forward for");
+    time = Forward(dist);
+    Serial.print(time);
+    Serial.println(" ms");
+    delay(1000);
+
+    //Turn Leg of each step
+    Serial.print("Step #:");
+    Serial.println(i + 1);
+    dir = moves[i + 1];
+    if (dir == LEFT) {
+      time = Turn(90);
+      Serial.print("turning LEFT ");
+      Serial.print(time);
+      Serial.println(" ms");
+    }
+    else {
+      time = Turn(-90);
+      Serial.println("turning RIGHT ");
+
+      Serial.print(time);
+      Serial.println(" ms");
+    } // end of else motions conditional
+    delay(1000);
+
+  } // end of for loop
+  Serial.println("That's All Folks!");
+  delay(1000);
+  exit(i);
+} // the end
+
+//////////////////////////////////////////////
+unsigned long Forward(int distance) {
+  unsigned long t;
+  t = distance * milliSecondsPerCM; //Time to keep motors on
+
+  //To drive forward, motors go in the same direction
+  run_motor(A, 197); //change PWM to your calibrations
+  run_motor(B, 229); //change PWM to your calibrations
+  delay(t);
+  run_motor(A, 0);
+  run_motor(B, 0);
+  return (t);
+}
+
+//////////////////////////////////////////////
+unsigned long Turn(int degrees) {
+  unsigned long t;
+  int sign = degrees / abs(degrees); //Find if left or right
+  t = (abs(degrees) / 90) * milliSecondsPer90Deg; //Time to keep motors on
+
+  //To turn, motors should spin in opposite directions
+  run_motor(A, sign * 200); //change PWM to your calibrations
+  run_motor(B, -sign * 210); //change PWM to your calibrations
+  delay(t);
+  run_motor(A, 0);
+  run_motor(B, 0);
+  return (t);
+
+}
